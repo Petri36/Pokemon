87aab65d292888b01f11bb035f7d324212bdb906
Petri36 <petriniagustin41521@gmail.com>
2023-02-06 23:34:25 -0300

    advancing database

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -1,12 +1,36 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('pokemon', {
-    name: {
+  sequelize.define("pokemon", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      unique: true,
+      allowNull: false,
+    },
+    nombre: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    vida: {
+      type: DataTypes.INTEGER,
+    },
+    ataque: {
+      type: DataTypes.INTEGER,
+    },
+    defensa: {
+      type: DataTypes.INTEGER,
+    },
+    velocidad: {
+      type: DataTypes.INTEGER,
+    },
+    altura: {
+      type: DataTypes.INTEGER,
+    },
+    peso: {
+      type: DataTypes.INTEGER,
     },
   });
 };
